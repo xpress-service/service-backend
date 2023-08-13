1faf0f547a779d4dcbfea5bf325cc70ff9f6f098
descmd1 <aondoakaachris@gmail.com>
2023-08-13 15:35:25 +0100

    merged signup to develop

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express  from 'express';
 import bodyParser  from 'body-parser';
 import path  from 'path';
-
 import dotenv from 'dotenv';
+
+
 dotenv.config();
 
+
+
 import userRouter  from './routes/userRoute.js';
+import authRouter  from './routes/auth.js';
+// import userRouter  from './routes/user.js';
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,6 +21,8 @@ to make api call in the frontend use
 localhost:{port}/api/users/signin
 */
 app.use('/api/users', userRouter)
+app.use('/api/auth', authRouter)
+// app.use('/api/users', userRouter)
 
 
 let nodeServer = app.listen(process.env.PORT, function () {
